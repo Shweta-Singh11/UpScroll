d455a5f714790126e60ed5729a88993dd36e2c31
Shweta <0311shweta@gmail.com>
2026-02-26 01:50:22 +0530

    Added About us Section

---
+++ src/pages/OurStory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StorySection from '../components/about/StorySection';
+
+const OurStory = () => {
+  return (
+    <div className="bg-black min-h-screen text-white pt-20">
+      <StorySection />
+    </div>
+  );
+};
+
+export default OurStory;
---
+++ src/pages/OurTeam.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeamSection from '../components/about/TeamSection';
+
+const OurTeam = () => {
+  return (
+    <div className="bg-black min-h-screen text-white pt-20">
+      <TeamSection />
+    </div>
+  );
+};
+
+export default OurTeam;
